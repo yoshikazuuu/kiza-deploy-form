18f670d2157972b9a755ba4476c1e75374fe103a
yoshikazuuu <febrianojerry@gmail.com>
2024-08-28 19:13:06 +0700

    feat: welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { ModeToggle } from "@/components/theme/theme-toggle";
 
 export default function Home() {
   return (
-    <div className="flex h-[100dvh] w-screen justify-center items-center">
+    <div className="flex h-[100dvh] w-screen justify-center flex-col items-center">
       <ModeToggle />
+      <span className="font-bold text-4xl">Welcome here, Kiza!</span>
     </div>
   );
 }
